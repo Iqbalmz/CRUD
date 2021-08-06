a297d518371d9c26f2a584fe600bc23d883fa9d1
Arifianto12RPLA <arifiantogik@gmail.com>
2021-08-06 16:36:15 +0700

    Tambah file index crud siswa

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+include('inc/config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Siswa</title>
+</head>
+<body>
+	<h1 align="center">Data Siswa</h1>
+	<table border="1" style="width: 100%; border-collapse: collapse;">
+		<tr>
+			<th style="width: 25px; color: white; background-color: black">#</th>
+			<th style="color: white; background-color: black">Foto</th>
+			<th style="width: 250px; color: white; background-color: black">Nama</th>
+			<th style="width: 180px; color: white; background-color: black">Telepon</th>
+			<th style="width: 100px; color: white; background-color: black">Jenis Kelamin</th>
+			<th style="width: 250px; color: white; background-color: black">Email</th>
+			<th style="width: 100px; ; color: white; background-color: black">Aksi</th>
+		</tr>
+	</table>
+</body>
+</html>
