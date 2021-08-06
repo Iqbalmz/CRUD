cfd39da0b4f16871aeaa0ce5aee8fcf54dc88092
Iqbalmz <86943103+Iqbalmz@users.noreply.github.com>
2021-08-06 15:24:32 +0530

    Update index.php
    menambah button tambah

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ include('inc/config.php');
 </head>
 <body>
 	<h1 align="center">Data Siswa</h1>
+	<a href="?act=Tambah">
+	<button type="button" style="background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;"> Tambah data </button>
+	</a>
 	<table border="1" style="width: 100%; border-collapse: collapse;">
 		<tr>
 			<th style="width: 25px; color: white; background-color: black">#</th>
